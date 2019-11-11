012c90b7c3ae629af14db69ed6051d96826db8e0
jumpei-sogawa <jumpe123oon@gmail.com>
2019-11-11 16:55:29 +0900

    develop meeting_entry form

--- app/controllers/meeting_entries_controller.rb
+++ app/controllers/meeting_entries_controller.rb
@@ -1,10 +1,26 @@
 class MeetingEntriesController < ApplicationController
   def new
+    @meeting_entry = MeetingEntry.new
   end
 
   def create
+    @meeting_entry = MeetingEntry.new(meeting_entry_params)
+
+    respond_to do |format|
+      if @meeting_entry.save
+        format.html { redirect_to root_path, notice: 'Meeting entry was successfully created.' }
+      else
+        format.html { redirect_to root_path }
+        format.json { render json: @meeting_entry.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def agreement
+  end
+
+  private
+  def meeting_entry_params
+    params.require(:meeting_entry).permit(:meeting_id, :family_name, :first_name, :family_kana, :first_kana, :phone, :email, :purpose, :note, :agreement)
   end
 end
