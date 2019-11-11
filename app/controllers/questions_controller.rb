f7c9a5b97ceb77b704061003b898af65074c57f3
jumpei-sogawa <jumpe123oon@gmail.com>
2019-11-11 22:27:23 +0900

    develop question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,27 @@
 class QuestionsController < ApplicationController
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to questions_confirmation_path
+    else
+      redirect_to root_path
+    end
   end
 
   def agreement
+  end
+
+  def confirmation
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:family_name, :first_name, :family_kana, :first_kana, :phone, :email, :content, :agreement)
+
   end
 end
